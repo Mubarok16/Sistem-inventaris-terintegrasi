8ede34fb86e24f64cac87a96eb082725e64e2862
farhan mubarok <mubarokf369@gmail.com>
2026-07-30 22:39:23 +0700

    perbaikan bug

--- app/Http/Controllers/PerawatanBarang.php
+++ app/Http/Controllers/PerawatanBarang.php
@@ -130,7 +130,7 @@ class PerawatanBarang extends Controller
             }
         } catch (\Exception $e) {
             // Jika gagal
-            return back()->with('gagal', 'Gagal menyimpan surat: pastikan nomor surat unik');
+            return back()->with('gagal', 'Gagal menyimpan surat: pastikan formulir, barang atau ruangan tidak boleh kosong!');
         }
     }
 
