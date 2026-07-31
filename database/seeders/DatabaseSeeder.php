98b561a4ed214286a27133849d161a5866cdda10
farhan mubarok <mubarokf369@gmail.com>
2026-08-01 00:43:13 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,85 +20,85 @@ class DatabaseSeeder extends Seeder
          // ==========================================
         // 1. DATA DOSEN
         // ==========================================
-        DB::table('users')->insert([
-            'id_user'   => 'USR-DSN001',
-            'username'  => 'kaproditkm',
-            'password'  => Hash::make('kaproditkm'), // Enkripsi password
-            'hak_akses' => 'kaprodi',
-            'status'    => 'aktif',
-            'created_at'=> now(),
-            'updated_at'=> now(),
-        ]);
+        // DB::table('users')->insert([
+        //     'id_user'   => 'USR-DSN001',
+        //     'username'  => 'kaproditkm',
+        //     'password'  => Hash::make('kaproditkm'), // Enkripsi password
+        //     'hak_akses' => 'kaprodi',
+        //     'status'    => 'aktif',
+        //     'created_at'=> now(),
+        //     'updated_at'=> now(),
+        // ]);
 
-        DB::table('detail_dosen')->insert([
-            'nidn'       => '0412038901',
-            'id_user'    => 'USR-DSN001',
-            'nama'       => 'kaprodi Teknik komputer',
-            'no_hp'      => null,
-            'jabatan'    => 'kaprodi Teknik komputer',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        // DB::table('detail_dosen')->insert([
+        //     'nidn'       => '0412038901',
+        //     'id_user'    => 'USR-DSN001',
+        //     'nama'       => 'kaprodi Teknik komputer',
+        //     'no_hp'      => null,
+        //     'jabatan'    => 'kaprodi Teknik komputer',
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
 
-        DB::table('users')->insert([
-            'id_user'   => 'USR-DSN002',
-            'username'  => 'kaprodispl',
-            'password'  => Hash::make('kaprodispl'), // Enkripsi password
-            'hak_akses' => 'kaprodi',
-            'status'    => 'aktif',
-            'created_at'=> now(),
-            'updated_at'=> now(),
-        ]);
+        // DB::table('users')->insert([
+        //     'id_user'   => 'USR-DSN002',
+        //     'username'  => 'kaprodispl',
+        //     'password'  => Hash::make('kaprodispl'), // Enkripsi password
+        //     'hak_akses' => 'kaprodi',
+        //     'status'    => 'aktif',
+        //     'created_at'=> now(),
+        //     'updated_at'=> now(),
+        // ]);
 
-        DB::table('detail_dosen')->insert([
-            'nidn'       => '0412038902',
-            'id_user'    => 'USR-DSN002',
-            'nama'       => 'kaprodi teknik sipil',
-            'no_hp'      => null,
-            'jabatan'    => 'kaprodi teknik sipil',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        // DB::table('detail_dosen')->insert([
+        //     'nidn'       => '0412038902',
+        //     'id_user'    => 'USR-DSN002',
+        //     'nama'       => 'kaprodi teknik sipil',
+        //     'no_hp'      => null,
+        //     'jabatan'    => 'kaprodi teknik sipil',
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
 
-        DB::table('users')->insert([
-            'id_user'   => 'USR-DSN003',
-            'username'  => 'kaproditl',
-            'password'  => Hash::make('kaproditl'), // Enkripsi password
-            'hak_akses' => 'kaprodi',
-            'status'    => 'aktif',
-            'created_at'=> now(),
-            'updated_at'=> now(),
-        ]);
+        // DB::table('users')->insert([
+        //     'id_user'   => 'USR-DSN003',
+        //     'username'  => 'kaproditl',
+        //     'password'  => Hash::make('kaproditl'), // Enkripsi password
+        //     'hak_akses' => 'kaprodi',
+        //     'status'    => 'aktif',
+        //     'created_at'=> now(),
+        //     'updated_at'=> now(),
+        // ]);
 
-        DB::table('detail_dosen')->insert([
-            'nidn'       => '0412038903',
-            'id_user'    => 'USR-DSN003',
-            'nama'       => 'kaprodi teknik lingkungan',
-            'no_hp'      => null,
-            'jabatan'    => 'kaprodi teknik lingkungan',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        // DB::table('detail_dosen')->insert([
+        //     'nidn'       => '0412038903',
+        //     'id_user'    => 'USR-DSN003',
+        //     'nama'       => 'kaprodi teknik lingkungan',
+        //     'no_hp'      => null,
+        //     'jabatan'    => 'kaprodi teknik lingkungan',
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
 
-        DB::table('users')->insert([
-            'id_user'   => 'USR-DSN004',
-            'username'  => 'wadek',
-            'password'  => Hash::make('wadek'), // Enkripsi password
-            'hak_akses' => 'kaprodi',
-            'status'    => 'aktif',
-            'created_at'=> now(),
-            'updated_at'=> now(),
-        ]);
+        // DB::table('users')->insert([
+        //     'id_user'   => 'USR-DSN004',
+        //     'username'  => 'wadek',
+        //     'password'  => Hash::make('wadek'), // Enkripsi password
+        //     'hak_akses' => 'kaprodi',
+        //     'status'    => 'aktif',
+        //     'created_at'=> now(),
+        //     'updated_at'=> now(),
+        // ]);
 
-        DB::table('detail_dosen')->insert([
-            'nidn'       => '0412038904',
-            'id_user'    => 'USR-DSN004',
-            'nama'       => 'Wadek',
-            'no_hp'      => null,
-            'jabatan'    => 'Wakil Dekan',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        // DB::table('detail_dosen')->insert([
+        //     'nidn'       => '0412038904',
+        //     'id_user'    => 'USR-DSN004',
+        //     'nama'       => 'Wadek',
+        //     'no_hp'      => null,
+        //     'jabatan'    => 'Wakil Dekan',
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
 
         // ==========================================
         // 2. DATA STAFF / ADMIN
@@ -126,27 +126,27 @@ class DatabaseSeeder extends Seeder
         // ==========================================
         // 3. DATA PEMINJAM (MAHASISWA/UMUM)
         // ==========================================
-        DB::table('users')->insert([
-            'id_user'   => 'USR-PMJ001',
-            'username'  => 'farhan',
-            'password'  => Hash::make('farhan'),
-            'hak_akses' => 'mahasiswa',
-            'status'    => 'aktif',
-            'created_at'=> now(),
-            'updated_at'=> now(),
-        ]);
+        // DB::table('users')->insert([
+        //     'id_user'   => 'USR-PMJ001',
+        //     'username'  => 'farhan',
+        //     'password'  => Hash::make('farhan'),
+        //     'hak_akses' => 'mahasiswa',
+        //     'status'    => 'aktif',
+        //     'created_at'=> now(),
+        //     'updated_at'=> now(),
+        // ]);
 
-        DB::table('peminjam')->insert([
-            'no_identitas'  => '22010304001',
-            'id_user'       => 'USR-PMJ001',
-            'nama_peminjam' => 'Farhan',
-            'fakultas'      => 'Teknik',
-            'prodi'         => 'Teknik Informatika',
-            'img_identitas' => 'ktm_farhan.jpg', // Nama file contoh gambar
-            'tahun_masuk'   => 2021,
-            'created_at'    => now(),
-            'updated_at'    => now(),
-        ]);
+        // DB::table('peminjam')->insert([
+        //     'no_identitas'  => '22010304001',
+        //     'id_user'       => 'USR-PMJ001',
+        //     'nama_peminjam' => 'Farhan',
+        //     'fakultas'      => 'Teknik',
+        //     'prodi'         => 'Teknik Informatika',
+        //     'img_identitas' => 'ktm_farhan.jpg', // Nama file contoh gambar
+        //     'tahun_masuk'   => 2021,
+        //     'created_at'    => now(),
+        //     'updated_at'    => now(),
+        // ]);
 
     }
 }
