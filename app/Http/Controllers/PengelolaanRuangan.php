27a5b630b84c1f85c7cf5b1f916127e7d1c828f0
farhan mubarok <mubarokf369@gmail.com>
2026-08-03 02:00:25 +0700

    perbaikan bug route

--- app/Http/Controllers/PengelolaanRuangan.php
+++ app/Http/Controllers/PengelolaanRuangan.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\TipeRuangan;
 use App\Models\DataRuangan;
-use App\Services\Admin\PengelolaanAgendaService;
+use App\services\Admin\PengelolaanAgendaService;
 use Illuminate\Support\Facades\Auth;
 use Dflydev\DotAccessData\Data;
 use Illuminate\Support\Facades\DB;
