a32b83298436d92ec794ee268b032eba4deaed2d
farhan mubarok <mubarokf369@gmail.com>
2026-08-03 20:00:47 +0700

    berubah lagi

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
@@ -49,15 +49,16 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID', env('AWS_KEY')), // Tambahkan fallback AWS_KEY jika ada
-            'secret' => env('AWS_SECRET_ACCESS_KEY', env('AWS_SECRET')), // Tambahkan fallback AWS_SECRET jika ada
+            'key' => env('AWS_ACCESS_KEY_ID', env('AWS_KEY')),
+            'secret' => env('AWS_SECRET_ACCESS_KEY', env('AWS_SECRET')),
             'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-            'bucket' => env('AWS_BUCKET', env('AWS_BUCKET_NAME')), // WAJIB: Tambahkan fallback AWS_BUCKET_NAME
+            'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+            'report' => false,
         ],
-
 
     ],
 
