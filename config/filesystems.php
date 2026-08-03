de6c88806428b9670add3d224aba87673498481e
farhan mubarok <mubarokf369@gmail.com>
2026-08-03 20:27:25 +0700

    sama

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,24 +41,37 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
         ],
 
+        // 's3' => [
+        //     'driver' => 's3',
+        //     'key' => env('AWS_ACCESS_KEY_ID', env('AWS_KEY')),
+        //     'secret' => env('AWS_SECRET_ACCESS_KEY', env('AWS_SECRET')),
+        //     'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+        //     'bucket' => env('AWS_BUCKET'),
+        //     'url' => env('AWS_URL'),
+        //     'endpoint' => env('AWS_ENDPOINT'),
+        //     'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+        //     'throw' => false,
+        //     'report' => false,
+        // ],
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID', env('AWS_KEY')),
             'secret' => env('AWS_SECRET_ACCESS_KEY', env('AWS_SECRET')),
             'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-            'bucket' => env('AWS_BUCKET'),
+            'bucket' => env('AWS_BUCKET', env('AWS_BUCKET_NAME')),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
+            'throw' => true, // Ubah menjadi true agar error sistem terlihat jelas
             'report' => false,
         ],
+
 
     ],
 
